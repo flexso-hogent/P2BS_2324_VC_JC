0ab32fe2a67b837cf3aa820f2abacd55672cfb3e
Volbeda Darian <darian.volbeda@student.hogent.be>
2024-04-28 23:44:18 +0200

    a bit of the flexible column layout

--- Back-end/app/flexso/webapp/controller/List.controller.js
+++ Back-end/app/flexso/webapp/controller/List.controller.js
@@ -4,13 +4,19 @@ sap.ui.define([
 	"sap/ui/model/Filter",
 	"sap/ui/model/FilterOperator",
 	'sap/ui/model/Sorter',
-	'sap/m/MessageBox'
-], function (JSONModel, Controller, Filter, FilterOperator, Sorter, MessageBox) {
+	'sap/m/MessageBox',
+	'sap/f/library'
+], function (JSONModel, Controller, Filter, FilterOperator, Sorter, MessageBox,fioriLibrary) {
 	"use strict";
 
 	return Controller.extend("flexso.controller.List", {
 		onInit: function () {
 		},
+		onListItemPress: function () {
+			var oFCL = this.oView.getParent().getParent();
+
+			oFCL.setLayout(fioriLibrary.LayoutType.TwoColumnsMidExpanded);
+		}
 
 	});
 });
