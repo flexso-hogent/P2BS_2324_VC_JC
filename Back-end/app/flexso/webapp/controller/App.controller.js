65887d0a003781868135b7f6c978f862cdec88ff
Jeremy Crossan <114576701+JeremyCrossan@users.noreply.github.com>
2024-04-30 16:44:07 +0200

    Foto's en searchbar toegevoegd

--- Back-end/app/flexso/webapp/controller/App.controller.js
+++ Back-end/app/flexso/webapp/controller/App.controller.js
@@ -1,11 +1,15 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    "sap/ui/core/UIComponent"
+    "sap/ui/core/UIComponent",
+    "sap/m/MessageToast",
+    "sap/ui/model/Filter",
+    "sap/ui/model/FilterOperator",
+    "sap/ui/core/Item"
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller, UIComponent) {
+    function (Controller, UIComponent, Filter, FilterOperator, Item, MessageToast) {
         "use strict";
 
         return Controller.extend("flexso.controller.App", {
@@ -13,12 +17,36 @@ sap.ui.define([
             onEventClick: function (oEvent) {
                 var oItem = oEvent.getSource();
                 var sEventID = oItem.getBindingContext().getProperty("eventID");
-    
+                
+               // MessageToast.show("Navigating to Event ID: " + sEventID);
+
                 // Navigeer naar de nieuwe view, geef de eventID door
                 var oRouter = UIComponent.getRouterFor(this);
                 oRouter.navTo("RouteEvent", {
                     eventID: sEventID
                 });
+            },
+
+            onLocationChange: function (oEvent) {
+                var sSelectedKey = oEvent.getParameter("selectedItem").getKey();
+                var oHBox = this.byId("eventsHBox");
+                var oBinding = oHBox.getBinding("items");
+        
+                if (sSelectedKey === "All") {
+                    oBinding.filter([]); // Geen filter als "All" is geselecteerd
+                } else {
+                    var oFilter = new Filter("locatie", FilterOperator.EQ, sSelectedKey);
+                    oBinding.filter([oFilter]);
+                }
+            },
+    
+            onSortChange: function (oEvent) {
+                var sSelectedKey = oEvent.getParameter("selectedItem").getKey();
+                var oHBox = this.byId("eventsHBox");
+                var oBinding = oHBox.getBinding("items");
+    
+                var oSorter = new sap.ui.model.Sorter("naam", sSelectedKey === "Descending");
+                oBinding.sort([oSorter]);
             }
         });
     });
