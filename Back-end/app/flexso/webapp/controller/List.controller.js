bd11fbf9889207d9a55857ebe810950426ffac4e
Tom Antjon <tomtjen@gmail.com>
2024-05-03 10:51:09 +0200

    Changed defaultModel to eventModel to prevent overwriting

--- Back-end/app/flexso/webapp/controller/List.controller.js
+++ Back-end/app/flexso/webapp/controller/List.controller.js
@@ -1,24 +1,40 @@
-sap.ui.define([
-	"sap/ui/model/json/JSONModel",
-	"sap/ui/core/mvc/Controller",
-	"sap/ui/model/Filter",
-	"sap/ui/model/FilterOperator",
-	'sap/ui/model/Sorter',
-	'sap/m/MessageBox',
-	'sap/f/library'
-], function (JSONModel, Controller, Filter, FilterOperator, Sorter, MessageBox,fioriLibrary) {
-	"use strict";
+sap.ui.define(
+  [
+    "sap/ui/model/json/JSONModel",
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/Filter",
+    "sap/ui/model/FilterOperator",
+    "sap/ui/model/Sorter",
+    "sap/m/MessageBox",
+    "sap/f/library",
+  ],
+  function (
+    JSONModel,
+    Controller,
+    Filter,
+    FilterOperator,
+    Sorter,
+    MessageBox,
+    fioriLibrary
+  ) {
+    "use strict";
 
-	return Controller.extend("flexso.controller.List", {
-		onInit: function () {
-			this.oRouter = this.getOwnerComponent().getRouter();
-		},
-		onListItemPress: function (oEvent) {
-			var productPath = oEvent.getSource().getBindingContext().getPath(),
-				product = productPath.split("/").slice(-1).pop();
+    return Controller.extend("flexso.controller.List", {
+      onInit: function () {
+        this.oRouter = this.getOwnerComponent().getRouter();
+      },
+      onListItemPress: function (oEvent) {
+        var productPath = oEvent
+            .getSource()
+            .getBindingContext("eventModel")
+            .getPath(),
+          product = productPath.split("/").slice(-1).pop();
 
-			this.oRouter.navTo("detail", {layout: fioriLibrary.LayoutType.TwoColumnsMidExpanded, eventID: product});
-		}
-
-	});
-});
+        this.oRouter.navTo("detail", {
+          layout: fioriLibrary.LayoutType.TwoColumnsMidExpanded,
+          eventID: product,
+        });
+      },
+    });
+  }
+);
--- Back-end/db/data-model.cds
+++ Back-end/db/data-model.cds
@@ -1,7 +1,7 @@
 namespace my.events;
 
 entity Events {
-  key eventID      : UUID;
+  key eventID      : Int16;
       naam         : String;
       beschrijving : String;
       datum        : DateTime;
@@ -11,7 +11,7 @@ entity Events {
 }
 
 entity Sessions {
-  key sessieID     : UUID;
+  key sessieID     : Int16;
       naam         : String;
       beschrijving : String;
       spreker      : String;
